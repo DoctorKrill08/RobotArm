fc02cbd88eb6f8a520ff50385714656934d891b0
DoctorKrill08 <doctorkrill08@gmail.com>
2026-05-01 22:41:27 -0400

    Setup???

---
+++ claw.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from dynamixel_sdk import *
+
+portHandler = PortHandler("COM3")  # TODO Find out what the port name for claw is
+packetHandler = PacketHandler(2.0)  # protocol version
+portHandler.openPort()
+portHandler.setBaudRate(57600) #Rate of updating, I am not sure what units of measurement this is
